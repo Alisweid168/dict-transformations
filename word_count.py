3bc8fbf4283304d24a3a660dcc6704b45c8f45fc
AliSweid168 <ali.sweidan003@gmail.com>
2025-02-28 21:39:55 +0200

    Implemented word count function to count word occurrences

---
+++ word_count.py
@@ -0,0 +1,9 @@
+def word_count(sentence):
+    words = sentence.split()
+    word_dict = {}
+    for word in words:
+        word_dict[word] = word_dict.get(word, 0) + 1
+    return word_dict
+
+sentence = "apple orange banana apple orange apple"
+print("Word Count:", word_count(sentence))
